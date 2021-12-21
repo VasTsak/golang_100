3bee8cdabeaa93e174524eaedf3b50238d82c3f1
Vasileios Tsakalos <vasileios.m.tsakalos@gmail.com>
2021-12-21 18:11:42 +0200

    Create for_loop_exercise.go

---
+++ Day 3/for_loop_exercise.go
@@ -0,0 +1,14 @@
+/*
+Create a for loop that iterates from 5 to 15 and prints the numbers
+*/
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	for i := 5; i < 16; i++ {
+		fmt.Println(i)
+	}
+}
